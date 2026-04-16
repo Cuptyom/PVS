2afdd7fd2c597d424719177257531d2ab1a36c3c
Cuptyom <154819320+Cuptyom@users.noreply.github.com>
2026-04-16 09:01:33 +0300

    коммит

---
+++ PassportVisaService/Forms/LoginForm.Designer.cs
@@ -0,0 +1,36 @@
+﻿namespace PassportVisaService.Forms
+{
+    partial class LoginForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 450);
+            this.Name = "LoginForm";
+            this.Text = "Вход в систему";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
